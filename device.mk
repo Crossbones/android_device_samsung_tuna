3ebb2e24e4f3e69e2c574d2a6933f1aeeb7ce3d0
Mike Wielgosz <dev.mwielgosz@gmail.com>
2013-02-22 22:27:43 -0500

    Replace ZenSERIES with Trinity Kernel Alpha-1 for android-4.2.2 Temporary change

--- device.mk
+++ device.mk
@@ -28,9 +28,7 @@ endif
 PRODUCT_COPY_FILES += \
     device/samsung/tuna/modules/ansi_cprng.ko:system/modules/ansi_cprng.ko \
     device/samsung/tuna/modules/rpmsg_client_sample.ko:system/modules/rpmsg_client_sample.ko \
-    device/samsung/tuna/modules/rpmsg_server_sample.ko:system/modules/rpmsg_server_sample.ko \
-    device/samsung/tuna/modules/scsi_wait_scan.ko:system/modules/scsi_wait_scan.ko \
-    device/samsung/tuna/sysctl.conf:system/etc/sysctl.conf
+    device/samsung/tuna/modules/rpmsg_server_sample.ko:system/modules/rpmsg_server_sample.ko
 
 DEVICE_PACKAGE_OVERLAYS := device/samsung/tuna/overlay
 
